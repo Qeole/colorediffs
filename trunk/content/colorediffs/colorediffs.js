039590bf1c0d76005d47e9465218561c9c61b2bc
vadim.atlygin <vadim.atlygin@b238571a-b723-0410-94d3-81b783fd7aea>
2007-01-08 23:25:12 +0000

    side-by-side mode introduced

--- trunk/content/colorediffs/colorediffs.js
+++ trunk/content/colorediffs/colorediffs.js
@@ -1,91 +1,267 @@
-function createStyle()
-{
-  var pref = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-  
-  var stylecontent="";
+function createStyle() {
+	var pref = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 
-  stylecontent += ".linetag {color: " + pref.getCharPref("diffColorer.anchor_fg") 
-                         + ";background-color: " + pref.getCharPref("diffColorer.anchor_bg") + ";}\n";
-  stylecontent += ".addline {color: " + pref.getCharPref("diffColorer.addedLine_fg") 
-                         + ";background-color: " + pref.getCharPref("diffColorer.addedLine_bg") + ";}\n";
-  stylecontent += ".delline {color: " + pref.getCharPref("diffColorer.deletedLine_fg") 
-                         + ";background-color: " + pref.getCharPref("diffColorer.deletedLine_bg") + ";}\n";
-  stylecontent += ".steadyline {color: " + pref.getCharPref("diffColorer.steadyLine_fg") 
-                         + ";background-color: " + pref.getCharPref("diffColorer.steadyLine_bg") + ";}\n";
-  stylecontent += ".title {color: " + pref.getCharPref("diffColorer.title_fg") 
-                         + ";background-color: " + pref.getCharPref("diffColorer.title_bg") + ";}\n";
-  //stylecontent += ".addline {color: red;}\n";
-  stylecontent += "pre {font-family:monospace;}\n"
+	var stylecontent="";
 
-  return stylecontent;
+	switch(pref.getCharPref("diffColorer.view-mode")) {
+		case "unified":
+			stylecontent += ".linetag {color: " + pref.getCharPref("diffColorer.anchor_fg")
+				+ ";background-color: " + pref.getCharPref("diffColorer.anchor_bg") + ";}\n";
+			stylecontent += ".addline {color: " + pref.getCharPref("diffColorer.addedLine_fg")
+				+ ";background-color: " + pref.getCharPref("diffColorer.addedLine_bg") + ";}\n";
+			stylecontent += ".delline {color: " + pref.getCharPref("diffColorer.deletedLine_fg")
+				+ ";background-color: " + pref.getCharPref("diffColorer.deletedLine_bg") + ";}\n";
+			stylecontent += ".steadyline {color: " + pref.getCharPref("diffColorer.steadyLine_fg")
+				+ ";background-color: " + pref.getCharPref("diffColorer.steadyLine_bg") + ";}\n";
+			stylecontent += ".title {color: " + pref.getCharPref("diffColorer.title_fg")
+				+ ";background-color: " + pref.getCharPref("diffColorer.title_bg") + ";}\n";
+			//stylecontent += ".addline {color: red;}\n";
+			stylecontent += "pre {font-family:monospace;}\n";
+			break;
+		case "side-by-side":
+			stylecontent += "	.log {color: " + pref.getCharPref("diffColorer.sbs_log_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_log_bg") + "; padding: 5px; border: 1px solid black;}";
+			stylecontent += "	.file-diff {color: " + pref.getCharPref("diffColorer.sbs_file-diff_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_file-diff_bg") + "; padding: 3px;}";
+			stylecontent += "	.title {color: " + pref.getCharPref("diffColorer.sbs_title_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_title_bg") + "; padding: 3px;clear:left;}";
+			stylecontent += "	.pre-code {color: " + pref.getCharPref("diffColorer.sbs_precode_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_precode_bg") + ";}";
+			stylecontent += "	.addline {color: " + pref.getCharPref("diffColorer.sbs_addedLine_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_addedLine_bg") + ";}";
+			stylecontent += "	.delline {color: " + pref.getCharPref("diffColorer.sbs_deletedLine_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_deletedLine_bg") + ";}";
+			stylecontent += "	.linetag {color: " + pref.getCharPref("diffColorer.sbs_anchor_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_anchor_bg") + ";text-align:center;clear:left;}";
+			stylecontent += "	.steadyline {color: " + pref.getCharPref("diffColorer.sbs_steadyLine_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_steadyLine_bg") + ";}";
+			stylecontent += "	.left {float:left; padding: 5px; margin:0; width:48%;overflow:auto; color: " + pref.getCharPref("diffColorer.sbs_left_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_left_bg") + "; border: 1px solid black;}";
+			stylecontent += "	.right {float:right; padding: 5px; margin:0; width:48%;overflow:auto; color: " + pref.getCharPref("diffColorer.sbs_right_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_right_bg") + "; border: 1px solid black;}";
+			stylecontent += "	.left-title {float:left; padding: 5px; padding-top:0; padding-bottom:0; margin:0; width:48%;overflow:auto; color: " + pref.getCharPref("diffColorer.sbs_left-title_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_left-title_bg") + "; border: 1px solid black;}";
+			stylecontent += "	.right-title {float:right; padding: 5px; padding-top:0; padding-bottom:0; margin:0; width:48%;overflow:auto; color: " + pref.getCharPref("diffColorer.sbs_right-title_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_right-title_bg") + "; border: 1px solid black;}";
+			stylecontent += "	.left .addline {width:100%; color: green; margin-right:5px; color: " + pref.getCharPref("diffColorer.sbs_emptyLine_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_emptyLine_bg") + ";}";
+			stylecontent += "	.right .delline {width:100%; color: green; color: " + pref.getCharPref("diffColorer.sbs_emptyLine_fg") + "; background-color:" + pref.getCharPref("diffColorer.sbs_emptyLine_bg") + ";}";
+			break;
+	}
+	return stylecontent;
 }
 
-function onLoadMessage()
-{
-  var content = getMessagePane();
-  if(!content) return;
-
-  var loadedMessage = GetLoadedMessage();
-  if(!loadedMessage) return;
-
-  var messagePrefix = /^mailbox-message:|^imap-message:|^news-message:|^file:/i;
-  if ( ! messagePrefix.test(loadedMessage) ) return;
-
-  var message = content.contentDocument;
-
-  var body = message.getElementsByTagName("body")[0];
-
-  if( !body ) {
-    return;
-  }
-
-  var divs = body.getElementsByTagName("div");
-  var div = null;
-
-  for ( var i=0; i < divs.length; i++ ) {
-	  switch(divs[i].getAttribute("class")) {
-		  case "moz-text-plain":
-		  case "moz-text-flowed":
-			  div = divs[i];
-			  break;
-		  default:
-	  }
-  }
-
-  if( !div )
-  {
-    return;
-  }
-
-  div.innerHTML = parseDiff(div.innerHTML);
-
-  var styleElement = message.createElement("style");
-  styleElement.type = "text/css";
-
-  var styletext = document.createTextNode(createStyle());
-  styleElement.appendChild(styletext);
-  
-  var head = message.getElementsByTagName("head")[0]
-  head.appendChild(styleElement);
-//  dump(div.innerHTML);
+function getDecoration() {
+	switch(pref.getCharPref("diffColorer.view-mode")) {
+		case "unified":
+			return normalDecoration;
+		case "side-by-side":
+			return sideBySideDecoration;
+	}
+	return normalDecoration;
 }
 
-function parseDiff(text) {
-   if (! isDiff(text)) return text;
-   var newText = text;
-   newText = newText.replace(/^(<pre.*>)/mg, "$1\n\n\n\n" );
-   newText = newText.replace(/^(@@\s\-\d+\,\d+\s\+\d+\,\d+\s@@)$/mg, "<span class='linetag'>$1</span>" );
-   newText = newText.replace(/^(\+[^+].*)$/mg, "<span class='addline'>$1</span>" );
-   newText = newText.replace(/^(\+{3}.*\d+\.\d+)$/mg, "<span class='addline'>$1</span>" );
-   newText = newText.replace(/^(\-[^-].*)$/mg, "<span class='delline'>$1</span>" );
-   newText = newText.replace(/^(\-{3}.*\d+\.\d+)$/mg, "<span class='delline'>$1</span>" );
-   newText = newText.replace(/^( .*)$/mg, "<span class='steadyline'>$1</span>" );
-   newText = newText.replace(/(\n\n)((.*\n){1,3}[-=]+\n)/g, "$1<div class='title'>$2</div>" );
-   return newText;
+function onLoadMessage() {
+	var content = getMessagePane();
+	if(!content) return;
+
+	var loadedMessage = GetLoadedMessage();
+	if(!loadedMessage) return;
+
+	var messagePrefix = /^mailbox-message:|^imap-message:|^news-message:|^file:/i;
+	if ( ! messagePrefix.test(loadedMessage) ) return;
+
+	var message = content.contentDocument;
+
+	var body = message.body;
+
+	if( !body ) {
+		return;
+	}
+
+	if (!isDiff(body.innerHTML)) {
+		return;
+	}
+
+	if ( pref.getBoolPref("diffColorer.debug-mode" )) {
+			var file = Components.classes["@mozilla.org/file/local;1"]
+				.createInstance(Components.interfaces.nsILocalFile);
+			file.initWithPath("%TEMP%\\before.html");
+			// file is nsIFile, data is a string
+			var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+				.createInstance(Components.interfaces.nsIFileOutputStream);
+
+			// use 0x02 | 0x10 to open file for appending.
+			foStream.init(file, 0x02 | 0x08 | 0x20, 0664, 0); // write, create, truncate
+			foStream.write(message.documentElement.innerHTML, message.documentElement.innerHTML.length);
+			foStream.close();
+	}
+
+	var divs = body.getElementsByTagName("div");
+	var div = null;
+
+	for ( var i=0; i < divs.length; i++ ) {
+		switch(divs[i].getAttribute("class")) {
+			case "moz-text-plain":
+			case "moz-text-flowed":
+				divs[i].innerHTML = realParseDiff(divs[i].innerHTML, getDecoration());
+				div = divs[i];
+			default:
+		}
+	}
+
+	if( !div ) {
+		return;
+	}
+
+	var styleElement = message.createElement("style");
+	styleElement.type = "text/css";
+
+	var styletext = document.createTextNode(createStyle());
+	styleElement.appendChild(styletext);
+
+	var head = message.getElementsByTagName("head")[0];
+	head.appendChild(styleElement);
+
+	if ( pref.getBoolPref("diffColorer.debug-mode" )) {
+		var file = Components.classes["@mozilla.org/file/local;1"]
+			.createInstance(Components.interfaces.nsILocalFile);
+		file.initWithPath("%TEMP%\\after.html");
+		// file is nsIFile, data is a string
+		var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"]
+			.createInstance(Components.interfaces.nsIFileOutputStream);
+
+		// use 0x02 | 0x10 to open file for appending.
+		foStream.init(file, 0x02 | 0x08 | 0x20, 0664, 0); // write, create, truncate
+		foStream.write(message.documentElement.innerHTML, message.documentElement.innerHTML.length);
+		foStream.close();
+	}
+}
+
+function getTestText() {
+	var text = "";
+	text = "Update of /data/cvs/updateagent\n";
+	text +="In directory frodo:/tmp/cvs-serv21674/updateagent\n";
+	text +="\n";
+	text +="Modified Files:\n";
+	text +="	UpdateAgent.cpp\n";
+	text +="Log Message:\n";
+	text +="Fix for bug 21675 \"entries in the log should have time stamps \"\n";
+	text +="http://frodo/cgi-bin/bugzilla/show_bug.cgi?id=21675\n";
+	text +=" http://localhost/cgi-bin/bugzilla/show_bug.cgi?id=21675\n";
+	text +="\n";
+	text +="\n";
+	text +="Index: UpdateAgent.cpp http://frodo/cgi-bin/viewcvs.cgi/updateagent/UpdateAgent.cpp http://localhost/cgi-bin/viewcvs.cgi/updateagent/UpdateAgent.cpp\n";
+	text +="===================================================================\n";
+	text +="RCS file: /data/cvs/updateagent/UpdateAgent.cpp,v\n";
+	text +="retrieving revision 1.4\n";
+	text +="retrieving revision 1.5\n";
+	text +="diff -u -d -r1.4 -r1.5\n";
+	text +="--- UpdateAgent.cpp	8 Jun 2005 13:58:09 -0000	1.4\n";
+	text +="+++ UpdateAgent.cpp	3 Jan 2007 05:08:17 -0000	1.5\n";
+	text +="@@ -71,6 +71,7 @@\n";
+	text +="\n";
+	text +=" BOOL\n";
+	text +=" UpdateAgent::InitInstance() {\n";
+	text +="+	LogStream::log().setTimeStamp(true);\n";
+	text +="	static Cms::FileMutexImpl oldUpdateAgentMutex( \"HeartCodeAlreadyRunning\" );\n";
+	text +="	if ( oldUpdateAgentMutex.isAlreadyExists() ) {\n";
+	text +="		LogStream::log() << \"Trying to close MicroSimFilesync( ie. Old UpdateAgent ).\" << endl;\n";
+	text +="\n";
+	return text;
+}
+
+function normalDecoration(string, type) {
+	switch(type) {
+		case 0: return "<div class='log'>"+string+"</div>";
+		case 1: return "<div class='title'>"+string+"</div>";
+		case 2: return "<div class='pre-code'>"+string+"</div>";
+		case 3:
+			string = string.replace(/^(\+{3}.*\d+\.\d+)$/mg, "<span class='addline'>$1</span>" );
+			string = string.replace(/^(\-{3}.*\d+\.\d+)$/mg, "<span class='delline'>$1</span>" );
+			return string;
+		case 4: return "<div class='linetag'>"+string+"</div>";
+		case 5:
+			string = string.replace(/^(\+[^+].*)$/mg, "<span class='addline'>$1</span>" );
+			string = string.replace(/^(\-[^-].*)$/mg, "<span class='delline'>$1</span>" );
+			string = string.replace(/^( .*)$/mg, "<span class='steadyline'>$1</span>" );
+			return string;
+	}
+	return string;
+}
+
+function sideBySideDecoration(string, type) {
+	switch(type) {
+		case 0: if (string == "" ) {return "";} else {return "<pre class='log'>"+string+"</pre>";}
+		case 1: return "<pre class='title'>"+string+"</pre>";
+		case 2:
+			string = string.replace(/^(\+{3}.*)$/mg, "</pre><div class='right-title'>$1</div><pre class='pre-code'>" );
+			string = string.replace(/^(\-{3}.*)$/mg, "</pre><div class='left-title'>$1</div><pre class='pre-code'>" );
+			return "<pre class='pre-code'>"+string+"</pre>";
+		case 3:
+			return "<div class='file-diff'>" + string + "</div>";
+		case 4: return "<div class='linetag'>"+string+"</div>";
+		case 5: //split code
+			var left = [];
+			var right = [];
+			var lines = string.split("\n");
+			lines.push("");
+			for (var i=0; i < lines.length; i++) {
+				if (/^(\-.*)$/.test(lines[i])) {
+					left.push("<div class='delline'>"+lines[i].substring(1)+" </div>");
+				} else if (/^(\+.*)$/.test(lines[i])) {
+					right.push("<div class='addline'>"+lines[i].substring(1)+" </div>");
+				} else {
+					if ( left.length < right.length ) {
+						for (var k = left.length; k < right.length; k++) {
+							left.push("<div class='addline'> </div>");
+						}
+					} else if ( left.length > right.length ) {
+						for (var k = right.length; k < left.length; k++) {
+							right.push("<div class='delline'> </div>");
+						}
+					}
+					var steadyline = "<div class='steadyline'>"+lines[i].substring(1)+" </div>";
+					left.push(steadyline);
+					right.push(steadyline);
+				}
+			}
+			left.pop(); right.pop();
+			return "<pre class='left'>"+left.join("")+"</pre><pre class='right'>"+right.join("")+"</pre>";
+	}
+	return string;
+}
+
+function main() {
+	text = getTestText();
+	return realParseDiff(text, sideBySideDecoration);
+}
+
+function realParseDiff(text, decoration) {
+	//if (! isDiff(text)) return text;
+	//remove tags Thunderbird inserted
+	text = text.replace(/^<pre.*><br><hr.*><br>/, "\n\n");
+	text = text.replace(/^<pre.*>/, "\n\n");
+	text = text.replace(/<\/pre>$/, "");
+	//
+	var diffs = text.split(/\n\n((?:.*\n){1,3}[-=]+\n)/);
+	//Ok, diffs[0] is log, put it away. But if first is false it is just a first code part so process it normally.
+	//diffs[odd] are titles
+	//diffs[even] are diffs themselves
+	var newText = decoration(diffs[0], 0) + "\n\n";
+	for ( var i = 1; i < diffs.length; i += 2 ) {
+		var file = decoration(diffs[i], 1);
+		file += parseDiffPart(diffs[i+1], decoration);
+		newText += decoration(file, 3);
+	}
+	return newText;
+}
+
+function parseDiffPart(diff, decoration) {
+	var parts = diff.split(/^(@@\s\-\d+\,\d+\s\+\d+\,\d+\s@@)$/m);
+	//parts[0] is some text before code
+	var newText = decoration(parts[0], 2);
+	//parts[odd] are tags
+	//parts[even] are code
+	for ( var i = 1; i < parts.length; i += 1 ) {
+		if ( i % 2 == 1 ) {
+			newText += decoration(parts[i], 4);
+		} else {
+			newText += decoration(parts[i], 5);
+		}
+	}
+	return newText;
 }
 
 function isDiff(text) {
-   //check if text has line tags
-   line_tag = /^@@\s\-\d+\,\d+\s\+\d+\,\d+\s@@$/m;
-   return line_tag.test(text);
+	//check if text has line tags
+	line_tag = /^@@\s\-\d+\,\d+\s\+\d+\,\d+\s@@$/m;
+	return line_tag.test(text);
 }
--- trunk/content/colorediffs/options.js
+++ trunk/content/colorediffs/options.js
@@ -1,59 +1,88 @@
-function checkOptions()
-{
-  savePrefs();
+var pref = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+var coloredElems = ["anchor", "steadyLine", "deletedLine", "addedLine", "title", "sbs_title", "sbs_log", "sbs_file-diff", "sbs_precode", "sbs_left-title", "sbs_right-title", "sbs_anchor", "sbs_left", "sbs_right", "sbs_addedLine", "sbs_deletedLine", "sbs_steadyLine", "sbs_emptyLine"];
 
-  observerService.notifyObservers(null, "colored-diff-update", null);
+function init() {
+	for ( var i = 0; i < coloredElems.length; i++ ) {
+		var bg = coloredElems[i] + "_bg";
+		var fg = coloredElems[i] + "_fg";
 
-  return true;
+		setColor(fg, pref.getCharPref("diffColorer." + fg));
+		setColor(bg, pref.getCharPref("diffColorer." + bg));
+	}
+
+	document.getElementById('view_mode').setAttribute('value', pref.getCharPref("diffColorer.view-mode"));
+
+	//update combobox
+	var menulist = document.getElementById('view');
+	var items = menulist.firstChild.childNodes;
+	for( var i=0; i < items.length; i++ ) {
+		if ( items[i].value == pref.getCharPref("diffColorer.view-mode") ) {
+			menulist.selectedItem = items[i];
+			break;
+		}
+	}
+
+	updatePreview();
+}
+
+function savePrefs() {
+	for ( var i = 0; i < coloredElems.length; i++ ) {
+		var bg = coloredElems[i] + "_bg";
+		var fg = coloredElems[i] + "_fg";
+
+		pref.setCharPref("diffColorer." + fg, getColor(fg));
+		pref.setCharPref("diffColorer." + bg, getColor(bg));
+	}
+	pref.setCharPref("diffColorer.view-mode", document.getElementById('view_mode').getAttribute('value'));
+}
+
+function checkOptions() {
+	savePrefs();
+	observerService.notifyObservers(null, "colored-diff-update", null);
+	return true;
 }
 
 
-function updatePreview()
-{
-  for ( var i = 0; i < coloredElems.length; i++ ) {
-    var element = coloredElems[i];
-	var elem = document.getElementById(element);
-	elem.style.color = getColor(element + "_fg");
-	elem.style.backgroundColor = getColor(element + "_bg");
-  }
+function updatePreview() {
+	for ( var i = 0; i < coloredElems.length; i++ ) {
+		var element = coloredElems[i];
+		var elem = document.getElementById(element);
+		elem.style.color = getColor(element + "_fg");
+		elem.style.backgroundColor = getColor(element + "_bg");
+	}
 }
 
-function OnColorChange(element)
-{
-  var istext = (element.id.lastIndexOf("_fg")!=0);
+function OnColorChange(element) {
+	var istext = (element.id.lastIndexOf("_fg")!=0);
+	var colorObj = { NoDefault:false, Type:istext?"Text":"Page", TextColor:0, PageColor:0, Cancel:false };
 
-  var colorObj = { NoDefault:false, Type:istext?"Text":"Page", TextColor:0, PageColor:0, Cancel:false };
+	window.openDialog("chrome://editor/content/EdColorPicker.xul", "_blank", "chrome,close,titlebar,modal", "", colorObj);
 
-  window.openDialog("chrome://editor/content/EdColorPicker.xul", "_blank", "chrome,close,titlebar,modal", "", colorObj);
+	if (colorObj.Cancel) {
+		return;
+	}
 
-  if (colorObj.Cancel) {
-    return;
-  }
+	var color = (istext)?colorObj.TextColor:colorObj.BackgroundColor;
+	setColor(element.id, color);
 
-  var color = (istext)?colorObj.TextColor:colorObj.BackgroundColor;
-
-  setColor(element.id, color);
-
-  updatePreview();
+	updatePreview();
 }
 
 
-function setColor(elementId, color)
-{
-  var element = document.getElementById(elementId);
-  var colorElement = element.getElementsByTagName("spacer")[0];
-  if (colorElement && color)
-  {
-     colorElement.style.backgroundColor = color;
-  }
+function setColor(elementId, color) {
+	var element = document.getElementById(elementId);
+	var colorElement = element.getElementsByTagName("spacer")[0];
+	if (colorElement && color) {
+		colorElement.style.backgroundColor = color;
+	}
 }
 
-function getColor(elementId)
-{
-  var element = document.getElementById(elementId);
-  var colorElement = element.getElementsByTagName("spacer")[0];
-  if (colorElement)
-  {
-     return colorElement.style.backgroundColor;
-  }
+function getColor(elementId) {
+	var element = document.getElementById(elementId);
+	var colorElement = element.getElementsByTagName("spacer")[0];
+	if (colorElement) {
+		return colorElement.style.backgroundColor;
+	} else {
+		return null;
+	}
 }
