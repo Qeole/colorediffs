aa8b4da06705d7159f7e0de7e6d02d9cf8002866
vadim.atlygin <vadim.atlygin@b238571a-b723-0410-94d3-81b783fd7aea>
2007-06-28 11:28:57 +0000

    Context view support new IL tree format

--- trunk/content/colorediffs/transformations/replace-file-names-transformation.js
+++ trunk/content/colorediffs/transformations/replace-file-names-transformation.js
@@ -1,7 +1,14 @@
 colorediffsGlobal.transformations["replace-file-names"] = {
 	run: function(il) {
+		function replace(name) {
+			il.log = il.log.replace(new RegExp("([\/\.a-zA-Z0-9-]*" + name + ")"), "<a href='#" + name + "'>$1</a>");
+		}
+
 		il.files.forEach(function(file) {
-				il.log = il.log.replace(new RegExp("([\/\.a-zA-Z0-9-]*" + file.name + ")"), "<a href='#" + file.name + "'>$1</a>");
+				replace(file['new'].name);
+				if (file['new'].name != file['old'].name) {
+					replace(file['old'].name);
+				}
 			});
 
 		return il;
