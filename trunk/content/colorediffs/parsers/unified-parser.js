a4452b548a396d8d0ff20cc5a536a91bf41e8e1a
vadim.atlygin <vadim.atlygin@b238571a-b723-0410-94d3-81b783fd7aea>
2007-07-06 13:06:04 +0000

    Uh, do not use mailer that sends added files with --- NEW FILE: title please. It just too hard to support properly :(

--- trunk/content/colorediffs/parsers/unified-parser.js
+++ trunk/content/colorediffs/parsers/unified-parser.js
@@ -408,7 +408,7 @@ colorediffsGlobal.parsers["unified"] = {
 
 			try {
 				while(true) {
-					while(!_test(blank_line()) && !_test(/^--- .* ---$/)) {
+					while(!_test(blank_line()) && !_test(/^--- .* ---$/) && !_test(/^Index: /)) {
 						code.push(_get());
 						status.push("A");
 						_next();
