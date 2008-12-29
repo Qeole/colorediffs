0d3ae2db11df34f2094ce986241a28bbef56f6dd
Vadim Atlygin <vadim.atlygin@gmail.com>
2008-12-29 02:58:32 +0000

    Fix options dialog

--- content/colorediffs/options/options-pref-callback.js
+++ content/colorediffs/options/options-pref-callback.js
@@ -1,23 +1,32 @@
 colorediffsGlobal.OptionsPrefCallbackModel = function(prefModel, callback) {
 	this.getBoolPref = function(prop) {
 		return prefModel.getBoolPref(prop);
-	}
+	};
 
 	this.setBoolPref = function(prop, value) {
 		prefModel.setBoolPref(prop, value);
 		callback();
-	}
+	};
+
+	this.getIntPref = function(prop) {
+		return prefModel.getIntPref(prop);
+	};
+
+	this.setIntPref = function(prop, value) {
+		prefModel.setIntPref(prop, value);
+		callback();
+	};
 
 	this.getCharPref = function(prop) {
 		return prefModel.getCharPref(prop);
-	}
+	};
 
 	this.setCharPref = function(prop, value) {
 		prefModel.setCharPref(prop, value);
 		callback();
-	}
+	};
 
 	this.prefHasUserValue = function(prop) {
 		return prefModel.prefHasUserValue(prop);
-	}
-}
+	};
+};
--- content/colorediffs/options/options-pref.js
+++ content/colorediffs/options/options-pref.js
@@ -1,38 +1,47 @@
 colorediffsGlobal.OptionsPrefModel = function(prefModel) {
 	var boolPrefs = {};
 	var charPrefs = {};
+	var intPrefs = {};
 
 	var getPref = function(hash, prop, defFunc) {
 		return (hash[prop] != undefined) ? hash[prop] : defFunc(prop);
-	}
+	};
 
 	var setPref = function(hash, prop, value) {
 		hash[prop] = value;
-	}
+	};
 
 	var hasPref = function(prop) {
-		return boolPrefs[prop] != undefined || charPrefs[prop] != undefined;
-	}
+		return boolPrefs[prop] != undefined || charPrefs[prop] != undefined || intPrefs[prop] != undefined;;
+	};
 
 	this.getBoolPref = function(prop) {
 		return getPref(boolPrefs, prop, prefModel.getBoolPref);
-	}
+	};
 
 	this.setBoolPref = function(prop, value) {
 		setPref(boolPrefs, prop, value);
-	}
+	};
+
+	this.getIntPref = function(prop) {
+		return getPref(intPrefs, prop, prefModel.getIntPref);
+	};
+
+	this.setIntPref = function(prop, value) {
+		setPref(intPrefs, prop, value);
+	};
 
 	this.getCharPref = function(prop) {
 		return getPref(charPrefs, prop, prefModel.getCharPref);
-	}
+	};
 
 	this.setCharPref = function(prop, value) {
 		setPref(charPrefs, prop, value);
-	}
+	};
 
 	this.prefHasUserValue = function(prop) {
 		return hasPref(prop) || prefModel.prefHasUserValue(prop);
-	}
+	};
 
 	this.saveToModel = function() {
 		for (var b in boolPrefs) {
@@ -42,5 +51,9 @@ colorediffsGlobal.OptionsPrefModel = function(prefModel) {
 		for (var c in charPrefs) {
 			prefModel.setCharPref(c, charPrefs[c]);
 		}
-	}
-}
+
+		for (var i in intPrefs) {
+			prefModel.setIntPref(c, intPrefs[i]);
+		}
+	};
+};
