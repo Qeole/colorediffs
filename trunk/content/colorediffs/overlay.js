3bfd2793e6e39b9b6aad7f862c54adbe453f0138
vadim.atlygin <vadim.atlygin@b238571a-b723-0410-94d3-81b783fd7aea>
2009-12-19 12:06:24 +0000

    Disable tests

--- trunk/content/colorediffs/overlay.js
+++ trunk/content/colorediffs/overlay.js
@@ -13,59 +13,59 @@ colorediffsGlobal.MailPaneConfigObserver = {
 };
 
 colorediffsGlobal.load = function() {
-    var me = colorediffsGlobal;
+	var me = colorediffsGlobal;
 
-    var gmessagepane = me.getMessagePane();
-    if (gmessagepane) {
+	var gmessagepane = me.getMessagePane();
+	if (gmessagepane) {
 	gmessagepane.addEventListener("load", function() {me.onLoadMessage();}, true);
-    }
-
-    var updateObserver = {
-	observe: function(subject, topic, data) {
-	    if(topic=="colored-diff-update" && me.isActive()) {
-		ReloadMessage();
-	    }
 	}
-    };
 
-    var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
-    observerService.addObserver(updateObserver, "colored-diff-update", false);
+	var updateObserver = {
+	observe: function(subject, topic, data) {
+		if(topic=="colored-diff-update" && me.isActive()) {
+		ReloadMessage();
+		}
+	}
+	};
 
-    var unload = function() {
+	var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+	observerService.addObserver(updateObserver, "colored-diff-update", false);
+
+	var unload = function() {
 	var pref = me.getPrefs();
 	pref.QueryInterface(Components.interfaces.nsIPrefBranchInternal);
 	pref.removeObserver("mail.pane_config.dynamic", me.MailPaneConfigObserver);
 
 	observerService.removeObserver(updateObserver, "colored-diff-update");
-    };
+	};
 
-    window.addEventListener("unload", unload, false);
+	window.addEventListener("unload", unload, false);
 
-//     Components.utils.import("resource://test-framework/main.js");
-//     run_tests("{282C3C7A-15A8-4037-A30D-BBEB17FFC76B}", "content/colorediffs/tests", document);
+//	   Components.utils.import("resource://test-framework/main.js");
+//	   run_tests("{282C3C7A-15A8-4037-A30D-BBEB17FFC76B}", "content/colorediffs/tests", document);
 
-    var pref = new colorediffsGlobal.Pref(colorediffsGlobal.getPrefs());
-    if (pref.debugDir.has()) {
-	//run tests
+	// var pref = new colorediffsGlobal.Pref(colorediffsGlobal.getPrefs());
+	// if (pref.debugDir.has()) {
+	// //run tests
 
-	var MY_ID = "{282C3C7A-15A8-4037-A30D-BBEB17FFC76B}";
-	var em = Components.classes["@mozilla.org/extensions/manager;1"].getService(Components.interfaces.nsIExtensionManager);
-	var file = em.getInstallLocation(MY_ID).getItemFile(MY_ID, "test-framework/main.js");
-	var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
-	var sstream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
-	fstream.init(file, -1, 0, 0);
-	sstream.init(fstream);
+	// var MY_ID = "{282C3C7A-15A8-4037-A30D-BBEB17FFC76B}";
+	// var em = Components.classes["@mozilla.org/extensions/manager;1"].getService(Components.interfaces.nsIExtensionManager);
+	// var file = em.getInstallLocation(MY_ID).getItemFile(MY_ID, "test-framework/main.js");
+	// var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
+	// var sstream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
+	// fstream.init(file, -1, 0, 0);
+	// sstream.init(fstream);
 
-	var data = "";
-	var str = sstream.read(4096);
-	while (str.length > 0) {data += str; str = sstream.read(4096);}
+	// var data = "";
+	// var str = sstream.read(4096);
+	// while (str.length > 0) {data += str; str = sstream.read(4096);}
 
-	sstream.close();
-	fstream.close();
+	// sstream.close();
+	// fstream.close();
 
-	eval(data);
-	run_tests(MY_ID, "content/colorediffs/tests/", document);
-    }
+	// eval(data);
+	// run_tests(MY_ID, "content/colorediffs/tests/", document);
+	// }
 };
 
 colorediffsGlobal.temp = function() {
