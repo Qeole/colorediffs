Qeole/colorediffs

commit 2c2be9319d289484f561f614b0207d2eb54bc246

Use hljs's cssSelector to colour blocks, skip links
Highlight.js has a cssSelector option to mark what block should be
highlighted. Instead of colouring all child elements of the <pre>
blocks, we can add a dedicated class name to all child text elements
(only) and use the option to only colour those.

The motivation for this change is to avoid colouring the links present
in the email body, to avoid removing the <a> mark up and keep the
possibility to click them (plus the style), as provided by Thunderbird.

The Git email signature is not plain text and hence is now skipped, so
we can remove the dedicated steps.

Fixes: #104
Co-developed-by: Leonardo Garcia <leonardo.garcia@linaro.org>

scripts/coloring.js
@@ -12,42 +12,28 @@ const coloring = {
          * nodes with .hljs class between these <pre> node and their children.
          */
         const preNodes = document.querySelectorAll("body > div > pre");
+
         for (const pre of preNodes) {
-            const code = document.createElement("CODE");
+            for (let i = 0; i < pre.childNodes.length; i++) {
+                if (pre.childNodes[i].nodeName === "#text") {
+                    const span = document.createElement("SPAN");
 
-            code.style.padding = "0";
-            transformations.setTabStyle(code, options.tabsize);
-            code.setAttribute("class", "hljs diff");
+                    span.style.padding = "0";
+                    transformations.setTabStyle(span, options.tabsize);
+                    span.setAttribute("class", "hljs diff colorediffs");
 
-            while (pre.childNodes.length > 0) {
-                code.appendChild(pre.childNodes[0]);
+                    span.appendChild(pre.childNodes[i]);
+                    pre.insertBefore(span, pre.childNodes[i]);
+                }
             }
-            pre.appendChild(code);
-        }
-
-        /*
-         * Signature handling:
-         * Search for a div with "moz-txt-sig" class set somewhere inside the plain
-         * text mail. This node holds all signatures. We will remove it from the
-         * DOM to exclude it from the highlighting and add it back afterwards.
-         */
-        let sigParentNode;
-        const signatureNodes = document.querySelectorAll("div.moz-text-plain code > div.moz-txt-sig");
-
-        if (signatureNodes.length) {
-            sigParentNode = signatureNodes[0].parentNode;
-            signatureNodes[0].remove();
         }
 
         /* Call library function, trigger highlighting */
-        hljs.configure({ ignoreUnescapedHTML: true });
+        hljs.configure({
+            ignoreUnescapedHTML: true,
+            cssSelector: ".colorediffs",
+        });
         hljs.highlightAll();
-
-        /* Re-add signature, highlighted with "comment" color */
-        if (signatureNodes.length) {
-            signatureNodes[0].className += " hljs-comment";
-            sigParentNode.appendChild(signatureNodes[0]);
-        }
 
         /* Replace spaces and tabs if required */
         if (options.spaces) {

commit 4df15bdaa7f51ffe1124d0f9a08989f866791aa4

Fix whitespace display, bump patch version number
In a (more or less) recent commit, we changed the way we're building the
nodes for adding the colours. This introduced a bug for displaying white
spaces, because we didn't reflect the changes when passing the root node
to process to the relevant function.

This went undetected for a while because I didn't immediately publish a
new release with the buggy commit. Let's fix it now, and cut a patch
release with the bug fix.

Fixes: 2c2be9319d28 ("Use hljs's cssSelector to colour blocks, skip links")

scripts/coloring.js
@@ -38,7 +38,7 @@ const coloring = {
         /* Replace spaces and tabs if required */
         if (options.spaces) {
             for (const pre of preNodes) {
-                transformations.replaceSpaces(pre.firstChild, options.tabsize);
+                transformations.replaceSpaces(pre, options.tabsize);
             }
         } else if (document.getElementsByClassName("cd-s").length) {
             transformations.restoreSpaces();

scripts/transformations.js
@@ -91,7 +91,7 @@ const transformations = {
             transformations.restoreSpaces();
         } else {
             for (const pre of document.querySelectorAll("body > div > pre")) {
-                transformations.replaceSpaces(pre.firstChild, options.tabsize);
+                transformations.replaceSpaces(pre, options.tabsize);
             }
         }
     },